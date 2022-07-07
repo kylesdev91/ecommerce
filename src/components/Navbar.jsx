569788620d596a573a81b3b343ffecf0870d61e5
Kyle Hernandez <kh2kt@kylesdev91yahoo.onmicrosoft.com>
2022-07-07 09:03:34 -0500

    container black

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  height: 60px;
+  background-color: black;
+`;
 
 const Navbar = () => {
-  return <div>navbar</div>;
+  return <Container>navbar</Container>;
 };
 
 export default Navbar;
