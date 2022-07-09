3169c78bece16fc18de0cfe685679750156cf336
Kyle Hernandez <kh2kt@kylesdev91yahoo.onmicrosoft.com>
2022-07-09 17:26:52 -0500

    the hover and items

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Categories from '../components/Categories';
+import Products from '../components/Products';
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Categories />
+      <Products />
     </div>
   );
 };
