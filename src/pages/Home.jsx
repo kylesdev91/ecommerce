801bae8bbc8b6ba5cc1c320b3119a981c1be7d0b
Kyle Hernandez <kh2kt@kylesdev91yahoo.onmicrosoft.com>
2022-07-09 10:10:06 -0500

    Get rid of extra info, udpated files and got items

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import Announcement from '../components/Announcment';
 import Navbar from '../components/Navbar';
-import Slider from '../components/Slider';
+import Categories from '../components/Categories';
 
 const Home = () => {
   return (
     <div>
-      <Announcement />
       <Navbar />
-      <Slider />
+      <Categories />
     </div>
   );
 };
