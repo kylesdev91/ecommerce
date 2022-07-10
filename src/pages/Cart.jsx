66cf16ed4abcb835785812a5a2760c19e2f61227
Kyle Hernandez <kh2kt@kylesdev91yahoo.onmicrosoft.com>
2022-07-10 11:39:18 -0500

    Cart 1.0

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+import Navbar from '../components/Navbar';
+
+const Container = styled.div``;
+
+const Cart = () => {
+  return (
+    <Container>
+      <Navbar />
+      cart
+    </Container>
+  );
+};
+
+export default Cart;
