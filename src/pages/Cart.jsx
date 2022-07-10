1beb5848a2dbdac1f8e4bf9d46a88b4500366d6e
Kyle Hernandez <kh2kt@kylesdev91yahoo.onmicrosoft.com>
2022-07-10 11:47:40 -0500

    Checkout 2.0

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,12 +2,39 @@ import styled from 'styled-components';
 import Navbar from '../components/Navbar';
 
 const Container = styled.div``;
+const Wrapper = styled.div`
+  padding: 20px;
+`;
+const Title = styled.h1`
+  font-weight: 300;
+  text-align: center;
+`;
+const Top = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const TopButton = styled.button`
+  padding: 10px;
+  font-weight: 600px;
+  cursor: pointer;
+`;
+
+const Bottom = styled.div``;
 
 const Cart = () => {
   return (
     <Container>
       <Navbar />
-      cart
+      <Wrapper>
+        <Title>YOUR BAG</Title>
+        <Top>
+          <TopButton>CONTINUE SHOPPING</TopButton>
+          <TopButton>CHECKOUT NOW</TopButton>
+        </Top>
+        <Bottom></Bottom>
+      </Wrapper>
     </Container>
   );
 };
