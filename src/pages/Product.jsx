b467bd20faa0c76b43735402288375a5e9509d67
Kyle Hernandez <kh2kt@kylesdev91yahoo.onmicrosoft.com>
2022-07-10 07:03:41 -0500

    Half way on the Product

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -2,32 +2,31 @@ import styled from 'styled-components';
 import Navbar from '../components/Navbar';
 
 const Container = styled.div``;
+
 const Wrapper = styled.div`
   padding: 50px;
   display: flex;
 `;
+
 const ImgContainer = styled.div`
   flex: 1;
 `;
+
 const Image = styled.img`
   width: 100%;
   height: 90vh;
   object-fit: cover;
 `;
+
 const InfoContainer = styled.div`
   flex: 1;
-  padding: 0px 50px;
 `;
-const Title = styled.h1`
-  font-weight: 200;
-`;
-const Desc = styled.p`
-  margin: 20px 0px;
-`;
-const Price = styled.span`
-  font-weight: 100;
-  font-size: 40px;
-`;
+
+const Title = styled.h1``;
+
+const Desc = styled.p``;
+
+const Price = styled.span``;
 
 const Product = () => {
   return (
